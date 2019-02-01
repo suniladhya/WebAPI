7605229303477d8a9a8d29369e9b690d5f2ba7df
Adhya <50034618@acsind.com>
2019-02-01 11:50:36 +0530

    JsonFormatter in weConfig,__DefaultRoute__NinJect__WEBAPIContrib_IOC Configured

---
+++ CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,50 @@
+﻿using CountingKs.Data;
+using CountingKs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private ModelFactory _modelFactory;
+
+        private ICountingKsRepository _repo { get; set; }
+        public FoodsController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+            _modelFactory = new ModelFactory();
+        }
+        public IEnumerable<FoodModel> Get()
+        {
+            var result = _repo.GetAllFoods()
+                              .OrderByDescending(f => f.Description)
+                              .Take(25)
+                              .ToList();
+
+            var resultWithMeasures = _repo.GetAllFoodsWithMeasures()
+                .OrderByDescending(x => x.Description)
+                .Take(25)
+                .ToList()
+                .Select(x=> new {
+                    Description = x.Description,
+                    Measures = x.Measures.Select(y=> new { Description = y.Description, Calories = y.Calories})
+                });
+            var resultMFWithMeasures = _repo.GetAllFoodsWithMeasures()
+                .OrderByDescending(f => f.Description)
+                .Take(25)
+                .ToList()
+                .Select(f => _modelFactory.Create(f));
+
+            return resultMFWithMeasures;
+        }
+        public FoodModel Get(int id)
+        {
+            return _modelFactory.Create(_repo.GetFood(id));
+        }
+    }
+}
---
+++ CountingKs/Models/ModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using CountingKs.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CountingKs.Models
+{
+    public class ModelFactory
+    {
+        public FoodModel Create(Food food)
+        {
+            return new FoodModel
+            {
+                Description = food.Description,
+                Measures = food.Measures.Select(x => Create(x))
+            };
+        }
+
+        private MeasureModel Create(Measure m)
+        {
+            return new MeasureModel
+            {
+                Description = m.Description,
+                Calories = Math.Round(m.Calories)
+            };
+        }
+    }
+}
